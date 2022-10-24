68f8d35f578dbbb91cc8069e8ebf22c0187f53e0
BarracudaLake <98210218+BarracudaLake@users.noreply.github.com>
2022-10-24 21:30:58 +0200

    new page

---
+++ sites/lime.barrabrowser.net.lua
@@ -0,0 +1,9 @@
+local web = website("Barracudas Browser")
+
+local Text = new("TextLabel",web)
+Text.Size = UDim2.fromScale(1,1)
+Text.Text = "Work in progress."
+Text.BackgroundTranseparency = 1
+Text.TextColor3 = Color3.new(1,1,1)
+
+return web
