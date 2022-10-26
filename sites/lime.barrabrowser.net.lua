05583b1783d8a6dc28eaf2d0b780b339cc9f06da
BarracudaLake <98210218+BarracudaLake@users.noreply.github.com>
2022-10-26 20:50:49 +0200

    Update lime.barrabrowser.net.lua

--- sites/lime.barrabrowser.net.lua
+++ sites/lime.barrabrowser.net.lua
@@ -3,7 +3,7 @@ local web = website("Barracudas Browser - The First LimeNet Browser")
 local Text = new("TextLabel",web)
 Text.Size = UDim2.fromScale(1,1)
 Text.Text = "Work in progress."
-Text.BackgroundTranseparency = 1
+Text.BackgroundTransparency = 1
 Text.TextColor3 = Color3.new(1,1,1)
 
 --it worked
