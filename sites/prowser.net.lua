cf77c0d3e5b609e9d5cbc4ea88f331c51715ca27
cytur <61370167+Cytur@users.noreply.github.com>
2022-11-08 21:23:02 -0500

    Update and rename lime.prowser.net.lua to prowser.net.lua

--- sites/prowser.net.lua
+++ sites/prowser.net.lua
@@ -3,5 +3,8 @@ local web = website("Prowser - Internet For All")
 local wip = new("TextLabel",web)
 wip.Size = UDim2.fromScale(1,1)
 wip.Text = "Site under maintenance"
+wip.BackgroundTransparency = 1
+wip.TextScaled = true
+wip.TextColor3 = Color3.fromRGB(255,255,255)
   
 return web
