fa7af24dd52b8b17ca56de398b2a3e4968d2d837
cytur <61370167+Cytur@users.noreply.github.com>
2022-11-11 22:51:32 -0500

    Create videopop.net.lua

---
+++ sites/videopop.net.lua
@@ -0,0 +1,16 @@
+-- website stuff
+local web = website("VideoPop")
+local nav = topbar()
+
+-- creating elements
+local logo = new("ImageLabel",nav)
+
+-- element editing
+logo.Size = UDim2.fromScale(0.1,1)
+logo.ScaleType = Enum.ScaleType.Fit
+logo.BackgroundTransparency = 1
+
+-- animations
+
+
+-- events
